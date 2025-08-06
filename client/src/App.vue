1c0e047a48621fd759e862aec26d236115056d3c
fatima-zz <felemenoun@gmail.com>
2025-08-06 20:00:19 +0100

    wip

---
+++ client/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1 class="title">Interactive reading assistant</h1>
+    <textarea name="text-analyze" id=""></textarea>
+  </main>
+</template>
+
+<style scoped>
+.title {
+  color: blueviolet;
+}
+</style>
